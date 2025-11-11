a0776ed67a7e4571aec66e4cb0246c7ae5be3906
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2025-11-11 14:54:00 -0800

    fix: remove deprecated BufferMaxEntries option from MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,6 @@ mongoose.connect(process.env.MONGO_URI, {
   maxPoolSize: 10, // Maximum number of connections.
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds.
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity.
-  bufferMaxEntries: 0 // Disable mongoose buffering.
 })
   .then(() => {
     console.log('connected to database')
